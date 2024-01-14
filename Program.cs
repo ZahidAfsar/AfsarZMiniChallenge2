43cd89a4e765d30e1d0a37858b10594fd21587a6
Zahid Afsar <zahidtempz@icloud.com>
2024-01-13 17:14:00 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Zahid Afsar
+// 9/7/23
+// Mini Challenge 2
+// The console asks for two numbers and then adds them together
+//Peer reviewed by Raudel Alvarado        Peer review comment : Everything runs good
+
+
+Console.WriteLine("What do you want to add?");
+Console.WriteLine("Enter first number ");
+
+
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Your first number is {firstNum}");
+
+
+Console.WriteLine("What is your second number?");
+
+
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"Your second number is {secondNum}");
+
+
+int total = firstNum + secondNum;
+
+Console.WriteLine($"{firstNum} + {secondNum} = {total}");
